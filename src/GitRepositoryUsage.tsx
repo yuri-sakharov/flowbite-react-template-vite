1334667f7f934d0c35613b5bfa1c0f6dbaea347e
Yuri <10849682+yuri-sakharov@users.noreply.github.com>
2026-05-11 11:51:06 +0300

    Code style fixed.

--- src/GitRepositoryUsage.tsx
+++ src/GitRepositoryUsage.tsx
@@ -1,6 +1,4 @@
-import {
-  useQuery,
-} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 import ky from "ky";
 
 interface GitHubRepository {
@@ -37,31 +35,31 @@ interface GitHubRepository {
   } | null;
   topics: string[];
   subscribers_count?: number; // Only returned by the single repo endpoint
-  network_count?: number;     // Only returned by the single repo endpoint
+  network_count?: number; // Only returned by the single repo endpoint
 }
 
 const getGitRepositoryData = async (): Promise<GitHubRepository> => {
-  const res = await ky('https://api.github.com/repos/oven-sh/bun')
-      return res.json();
+  const res = await ky("https://api.github.com/repos/oven-sh/bun");
+  return res.json();
 };
 
 export const GitRepositoryUsage = () => {
   const { isPending, error, data } = useQuery({
-    queryKey: ['repoData'],
+    queryKey: ["repoData"],
     queryFn: getGitRepositoryData,
-  })
+  });
 
-  if (isPending) return 'Loading...'
+  if (isPending) return "Loading...";
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <div>
       <h1>{data.name}</h1>
       <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{' '}
-      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
       <strong>🍴 {data.forks_count}</strong>
     </div>
-  )
-}
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeInit />
     <QueryClientProvider client={queryClient}>
-    <App />
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 );
