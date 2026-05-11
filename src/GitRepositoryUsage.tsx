e3401700a8133075fb24ae6ecbcbdeaf23eeb8d1
Yuri <10849682+yuri-sakharov@users.noreply.github.com>
2026-05-11 11:56:48 +0300

    Moved empty spaces.

--- src/GitRepositoryUsage.tsx
+++ src/GitRepositoryUsage.tsx
@@ -57,9 +57,9 @@ export const GitRepositoryUsage = () => {
     <div className="dark:text-white">
       <h1>{data.name}</h1>
       <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{" "}
-      <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong>
+      <strong>👀 {data.subscribers_count} </strong>
+      <strong>✨ {data.stargazers_count} </strong>
+      <strong>🍴 {data.forks_count} </strong>
     </div>
   );
 };
